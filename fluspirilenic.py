183f1b8be3678e126f7fe5b353e1f4ed767d0d6d
Ludovic Chabant <ludovic@chabant.com>
2013-08-15 08:19:49 -0700

    Added `--min_uid` parameter. Added `count` command. Some error checking.

--- fluspirilenic.py
+++ fluspirilenic.py
@@ -15,7 +15,9 @@ def run():
     parser.add_argument('--map', help="A file mapping the mailboxes to process. By default it will process both inboxes.")
     parser.add_argument('--debug', action='store_true', default=False, help="Show debug logging")
     parser.add_argument('--limit', default=-1, type=int, help="The maxiumum number of messages to process")
+    parser.add_argument('--min_uid', default=-1, type=int, help="The minimum UID to start from")
     subparsers = parser.add_subparsers()
+
     parser_read = subparsers.add_parser('read', help="Synchronize the read status of messages")
     parser_read.add_argument(
             '--mode',
@@ -23,6 +25,14 @@ def run():
             default='unread',
             help="What to sync:\n`read` means read messages on source will be flagged read on destination.\n`unread` means unread messages on source will be flagged unread on destination.")
     parser_read.set_defaults(func=sync_read)
+
+    parser_count = subparsers.add_parser('count', help="Counts messages with some flags")
+    parser_count.add_argument('--src_mbox', help="The source mailbox")
+    parser_count.add_argument('--dst_mbox', help="The destination mailbox")
+    parser_count.add_argument(
+            '--flags',
+            help="The flags to search for")
+    parser_count.set_defaults(func=count_flags)
     
     args = parser.parse_args()
     if args.debug:
@@ -57,13 +67,28 @@ def sync_read(args):
                 logger.debug("...got %d messages." % total)
 
                 for i, num in enumerate(msgnums):
+                    if args.min_uid > 0 and int(num) < args.min_uid:
+                        continue
+
                     typ, data = src.fetch(num, '(UID FLAGS BODY.PEEK[HEADER.FIELDS (Message-ID)])')
                     hd, val = _get_header(data[0][1])
-                    if hd != 'Message-ID':
+                    if hd.lower() != 'Message-ID'.lower():
                         raise Exception("Expected Message-ID, got %s" % hd)
+                    if not val or val == "":
+                        logger.warning("Ignoring message because it doesn't have a Message-ID:")
+                        typ, data = src.fetch(num, '(BODY.PEEK[HEADER])')
+                        logger.warning(data[0])
+                        continue
 
-                    typ, data = dst.search(None, 'HEADER', 'Message-ID', '"%s"' % val)
-                    dstnum = data[0]
+                    try:
+                        typ, data = dst.search(None, 'HEADER', 'Message-ID', '"%s"' % val)
+                        dstnum = data[0]
+                    except Exception as err:
+                        logger.warning("Ignoring message because search on destination failed:")
+                        logger.warning(err)
+                        typ, data = src.fetch(num, '(BODY.PEEK[HEADER])')
+                        logger.warning(data[0])
+                        continue
 
                     logger.debug("  Syncing status for %s -> %s [%s] [%s \\Seen]" % (num, dstnum, val, store_action))
                     dst.store(dstnum, store_action, '\\Seen')
@@ -80,22 +105,48 @@ def sync_read(args):
         _disconnect(src, dst)
 
 
-def _connect(args):
-    if not args.src or not args.dest:
-        raise Exception("You must specify a source and a destination.")
+def count_flags(args):
+    src, dst = _connect(args, bool(args.src), bool(args.dest))
+    try:
+        if src:
+            src.select(args.src_mbox or 'INBOX')
+            typ, data = src.search(None, args.flags)
+            msgnums = data[0].split()
+            logger.info("Found %d messages in source with flags %s" % (len(msgnums), args.flags))
+        if dst:
+            dst.select(args.dst_mbox or 'INBOX')
+            typ, data = dst.search(None, args.flags)
+            msgnums = data[0].split()
+            logger.info("Found %d messages in destination with flags %s" % (len(msgnums), args.flags))
+    finally:
+        _disconnect(src, dst)
 
-    src_server, src_user, src_pwd = _get_credentials(args.src)
-    dst_server, dst_user, dst_pwd = _get_credentials(args.dest)
+
+def _connect(args, connect_src=True, connect_dst=True):
+    if connect_src and not args.src:
+        raise Exception("You must specify a source")
+    if connect_dst and not args.dest:
+        raise Exception("You must specify a destination.")
+
     IMAP = imaplib.IMAP4
     if args.ssl:
         IMAP = imaplib.IMAP4_SSL
 
-    logger.info("Connecting to %s" % src_server)
-    src = IMAP(src_server)
-    src.login(src_user, src_pwd)
-    logger.info("Connecting to %s" % dst_server)
-    dst = IMAP(dst_server)
-    dst.login(dst_user, dst_pwd)
+    if connect_src:
+        src_server, src_user, src_pwd = _get_credentials(args.src)
+        logger.info("Connecting to %s" % src_server)
+        src = IMAP(src_server)
+        src.login(src_user, src_pwd)
+    else:
+        src = None
+
+    if connect_dst:
+        dst_server, dst_user, dst_pwd = _get_credentials(args.dest)
+        logger.info("Connecting to %s" % dst_server)
+        dst = IMAP(dst_server)
+        dst.login(dst_user, dst_pwd)
+    else:
+        dst = None
     
     return src, dst
 
