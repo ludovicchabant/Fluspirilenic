e37c4e490c948ca26e5083cfec44484d66c9d6ab
Ludovic Chabant <ludovic@chabant.com>
2013-08-16 23:44:16 -0700

    Added support for multi-mapping. Added `list` and `move` commands. Fixed a bug where some commands would flag messages as read. Some code refactoring.

--- fluspirilenic.py
+++ fluspirilenic.py
@@ -1,10 +1,10 @@
 import argparse
 import imaplib
 import logging
+from ConfigParser import SafeConfigParser
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
 
 
 def run():
@@ -12,12 +12,20 @@ def run():
     parser.add_argument('--src', help="The source account: <server>/<user>/<pwd>")
     parser.add_argument('--dest', help="The destination account: <server>/<user>/<pwd>")
     parser.add_argument('--ssl', action='store_true', default=False, help="Use SSL connection")
+    parser.add_argument('--config', help="Points to a config file that defines the source and destination")
     parser.add_argument('--map', help="A file mapping the mailboxes to process. By default it will process both inboxes.")
     parser.add_argument('--debug', action='store_true', default=False, help="Show debug logging")
+    parser.add_argument('--log', help="Log file to use")
     parser.add_argument('--limit', default=-1, type=int, help="The maxiumum number of messages to process")
     parser.add_argument('--min_uid', default=-1, type=int, help="The minimum UID to start from")
     subparsers = parser.add_subparsers()
 
+    # List mailboxes
+    parser_list = subparsers.add_parser('list', help="Lists mailboxes")
+    parser_list.add_argument('--use_dest', action='store_true', default=False, help="Use the destination account instead of the source one")
+    parser_list.set_defaults(func=list_mboxes)
+
+    # Sync read/unread statuses
     parser_read = subparsers.add_parser('read', help="Synchronize the read status of messages")
     parser_read.add_argument(
             '--mode',
@@ -26,18 +34,45 @@ def run():
             help="What to sync:\n`read` means read messages on source will be flagged read on destination.\n`unread` means unread messages on source will be flagged unread on destination.")
     parser_read.set_defaults(func=sync_read)
 
-    parser_count = subparsers.add_parser('count', help="Counts messages with some flags")
-    parser_count.add_argument('--src_mbox', help="The source mailbox")
-    parser_count.add_argument('--dst_mbox', help="The destination mailbox")
-    parser_count.add_argument(
-            '--flags',
-            help="The flags to search for")
+    # Move messages
+    parser_move = subparsers.add_parser('move', help="Move messages from one mailbox to another based on another server's mailbox")
+    parser_move.add_argument('--ref_mbox', help="The reference mailbox")
+    parser_move.add_argument('--from_mbox', nargs='+', help="The mailbox where messages will be taken from")
+    parser_move.add_argument('--to_mbox', help="The mailbox where messages will be moved to")
+    parser_move.set_defaults(func=move_msgs)
+
+    # Count messages
+    parser_count = subparsers.add_parser('count', help="Counts messages")
+    parser_count.add_argument('--use_dest', action="store_true", default=False, help="Use the destination account instead of the source one")
+    parser_count.add_argument('--mbox', help="The source mailbox")
+    parser_count.add_argument('--flags', help="The flags to filter with")
     parser_count.set_defaults(func=count_flags)
     
     args = parser.parse_args()
+
+    log_level = logging.INFO
     if args.debug:
-        logger.setLevel(logging.DEBUG)
+        log_level = logging.DEBUG
+    log_file = None
+    if args.log:
+        log_file = args.log
+    logging.basicConfig(level=log_level, filename=log_file)
+
     args.func(args)
+
+
+def list_mboxes(args):
+    src, dst = _connect(args, not args.use_dest, args.use_dest)
+    acc = dst if args.use_dest else src
+    if acc:
+        try:
+            data = acc.list()
+            for mbox in data[1]:
+                logger.info(mbox.split('"')[-2])
+        except Exception as ex:
+            logger.error(ex)
+        finally:
+            _disconnect(acc)
 
 
 def sync_read(args):
@@ -57,11 +92,15 @@ def sync_read(args):
         for mbox in mboxes:
             logger.info("Syncing %s -> %s" % (mbox[0], mbox[1]))
             src.select(mbox[0])
-            dst.select(mbox[1])
 
             try:
-                logger.debug("Getting destination message IDs...")
-                dst_msgids = _get_message_ids(dst)
+                dst_msgids_all = {}
+                for m in mbox[1]:
+                    logger.debug("Getting destination message IDs (%s)..." % m)
+                    dst.select(m)
+                    dst_msgids = _get_message_ids(dst)
+                    dst_msgids_all[m] = dst_msgids
+                    logger.debug("...got %s messages." % len(dst_msgids))
 
                 logger.debug("Searching for messages with flag '%s'..." % search_flag)
                 typ, data = src.search(None, search_flag)
@@ -83,9 +122,12 @@ def sync_read(args):
                         logger.warning(data[0])
                         continue
 
-                    try:
-                        dstnum = dst_msgids[val]
-                    except Exception:
+                    for m, msgids in dst_msgids_all.iteritems():
+                        dstnum = msgids.get(val)
+                        if dstnum is not None:
+                            dst.select(m)
+                            break
+                    else:
                         logger.warning("Ignoring message because we can't find it on the destination:")
                         logger.warning(val)
                         typ, data = src.fetch(num, '(BODY.PEEK[HEADER])')
@@ -102,30 +144,102 @@ def sync_read(args):
 
                     if float(i) / 100.0 == int(i / 100):
                         logger.info("Synced %d messages so far" % i)
+            except Exception as ex:
+                logger.error(ex)
             finally:
-                _close(src, dst)
+                _close(dst)
+    except Exception as ex:
+        logger.error(ex)
+    finally:
+        _disconnect(src, dst)
+
+
+def move_msgs(args):
+    if not args.ref_mbox or not args.from_mbox or not args.to_mbox:
+        raise Exception("You must specify the reference mailbox, from mailbox, and to mailbox")
+    src, dst = _connect(args)
+    try:
+        logger.debug("Getting source messages from %s" % args.ref_mbox)
+        src.select(args.ref_mbox)
+        ref_msgids = _get_message_ids(src)
+        logger.debug("...got %d messages" % len(ref_msgids))
+
+        from_msgids_all = {}
+        for mbox in args.from_mbox:
+            logger.debug("Getting destination messages from %s" % mbox)
+            dst.select(mbox)
+            from_msgids = _get_message_ids(dst)
+            logger.debug("...got %d messages" % len(from_msgids))
+            from_msgids_all[mbox] = from_msgids
+
+        i = 0
+        for ref_mid, ref_uid in ref_msgids.iteritems():
+            for mbox, msgids in from_msgids_all.iteritems():
+                from_uid = msgids.get(ref_mid)
+                if from_uid is not None:
+                    dst.select(mbox)
+                    break
+            else:
+                logger.warning("Ignoring message because we can't find it on the destination:")
+                logger.warning(ref_mid)
+                typ, data = src.fetch(ref_uid, '(BODY.PEEK[HEADER])')
+                logger.warning(data[0])
+                logger.warning("-----------")
+                continue
+
+            logger.debug("  Moving %s" % ref_mid)
+            dst.copy(from_uid, args.to_mbox)
+            dst.store(from_uid, '+FLAGS.SILENT', '\\Deleted')
+
+            i += 1
+            if args.limit > 0 and i >= args.limit:
+                logger.info("Reached limit of %d." % args.limit)
+                break
+
+            if float(i) / 100.0 == int(i / 100):
+                logger.info("Processed %d messages so far" % i)
+        dst.expunge()
+        dst.close()
+    except Exception as ex:
+        logger.error(ex)
     finally:
         _disconnect(src, dst)
 
 
 def count_flags(args):
-    src, dst = _connect(args, bool(args.src), bool(args.dest))
-    try:
-        if src:
-            src.select(args.src_mbox or 'INBOX')
-            typ, data = src.search(None, args.flags)
+    src, dst = _connect(args, not args.use_dest, args.use_dest)
+    acc = dst if args.use_dest else src
+    if acc:
+        try:
+            mbox = args.mbox or 'INBOX'
+            acc.select(mbox)
+            flags = args.flags or 'ALL'
+            typ, data = acc.search(None, flags)
             msgnums = data[0].split()
-            logger.info("Found %d messages in source with flags %s" % (len(msgnums), args.flags))
-        if dst:
-            dst.select(args.dst_mbox or 'INBOX')
-            typ, data = dst.search(None, args.flags)
-            msgnums = data[0].split()
-            logger.info("Found %d messages in destination with flags %s" % (len(msgnums), args.flags))
-    finally:
-        _disconnect(src, dst)
+            logger.info("Found %d messages in %s (searched for %s)" % (len(msgnums), mbox, flags))
+        except Exception as ex:
+            logger.error(ex)
+        finally:
+            _disconnect(acc)
 
 
 def _connect(args, connect_src=True, connect_dst=True):
+    if args.config:
+        c = SafeConfigParser()
+        c.read(args.config)
+        if not args.src:
+            srv = c.get('source', 'server')
+            usr = c.get('source', 'username')
+            pwd = c.get('source', 'password')
+            args.src = "%s/%s/%s" % (srv, usr, pwd)
+        if not args.dest:
+            srv = c.get('destination', 'server')
+            usr = c.get('destination', 'username')
+            pwd = c.get('destination', 'password')
+            args.dest = "%s/%s/%s" % (srv, usr, pwd)
+        if c.get('options', 'ssl'):
+            args.ssl = True
+
     if connect_src and not args.src:
         raise Exception("You must specify a source")
     if connect_dst and not args.dest:
@@ -164,17 +278,17 @@ def _get_mailboxes(args):
     for line in lines:
         if not line.strip():
             continue
-        mapping = map(lambda s: s.strip(), line.split(',', 2))
+        mapping = map(lambda s: s.strip(), line.split(','))
         if len(mapping) > 1:
-            mboxes.append(tuple(mapping))
+            mboxes.append((mapping[0], mapping[1:]))
         else:
-            mboxes.append((mapping[0], mapping[0]))
+            mboxes.append((mapping[0], [mapping[0]]))
         logger.debug("  %s -> %s" % (mboxes[-1][0], mboxes[-1][1]))
     return mboxes
 
 
 def _get_message_ids(box):
-    typ, data = box.fetch("1:*", "(BODY[HEADER.FIELDS (Message-Id)])")
+    typ, data = box.fetch("1:*", "(BODY.PEEK[HEADER.FIELDS (Message-Id)])")
     msgids = {}
     for d in data:
         if len(d) == 1:
@@ -186,14 +300,16 @@ def _get_message_ids(box):
     return msgids
 
 
-def _close(src, dst):
+def _close(src, dst=None):
     src.close()
-    dst.close()
+    if dst:
+        dst.close()
 
 
-def _disconnect(src, dst):
+def _disconnect(src, dst=None):
     src.logout()
-    dst.logout()
+    if dst:
+        dst.logout()
 
 
 def _get_credentials(creds):
